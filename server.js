69c89210acd864890c9762ea6c9658fa8df0636c
FatCat32 <antoine.mctaggart@gmail.com>
2017-02-02 00:36:11 -0500

    trying to fix heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
   if (req.headers['x-forward-proto'] === 'http') {
@@ -12,6 +12,6 @@ app.use(function (req, res, next) {
   }
 });
 
-app.listen(port, function () {
-  console.log('Express server is up on port ' + port);
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
